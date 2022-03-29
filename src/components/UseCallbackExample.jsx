78a655e5097912451d0da9009323c9c7854e0be7
Ethan Salonga <ethansalonga28@gmail.com>
2022-03-29 11:09:04 -0400

    useCallback example

---
+++ src/components/UseCallbackExample.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback } from "react"
+
+function UseCallbackExample() {
+  const [tasks, setTasks] = useState([])
+
+  const addTask = useCallback(() => {
+    setTasks((prevState) => [...prevState, "Some Task"])
+  }, [setTasks])
+
+  return (
+    <div>
+      <Button className="btn btn-primary" addTask={addTask} />
+      {tasks.map((task, index) => (
+        <p key={index}>{task}</p>
+      ))}
+    </div>
+  )
+}
+
+const Button = React.memo(({ addTask }) => {
+  console.log("Button rendered")
+  return (
+    <div>
+      <button onClick={addTask}>Add Task</button>
+    </div>
+  )
+})
+
+export default UseCallbackExample
